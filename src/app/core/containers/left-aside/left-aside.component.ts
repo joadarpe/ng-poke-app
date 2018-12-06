91b275c52b6f4850bdf2f78bdf09f3d9831a1bca
jonathana <jaap5227@gmail.com>
2018-12-06 16:50:41 -0500

    Getting user information from auth service

--- src/app/core/containers/left-aside/left-aside.component.ts
+++ src/app/core/containers/left-aside/left-aside.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'firebase/app';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-left-aside',
@@ -13,15 +15,21 @@ export class LeftAsideComponent implements OnInit {
     picUrl: string
   }
 
-  constructor() {
+  constructor(private authService: AuthService) {
     this.user = {
-      name: 'Jonathan',
-      email: 'jonathan@bizagi.com',
+      name: '',
+      email: '',
       picUrl: 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png'
     }
   }
 
   ngOnInit() {
+    this.authService.profileUser().subscribe(u => {
+      if (u) {
+        this.user.name = u.displayName
+        this.user.email = u.email
+      }
+    });
   }
 
 }
